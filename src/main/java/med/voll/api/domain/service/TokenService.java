a40d375fc83395cd982e2e47ae555fdf0bf7051a
Antonio Fagner S. de Oliveira <antonio-fagner.oliveira@serpro.gov.br>
2025-04-23 16:49:03 -0300

    05 - Controle de Acesso

--- src/main/java/med/voll/api/domain/service/TokenService.java
+++ src/main/java/med/voll/api/domain/service/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.domain.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,17 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-
     @Value("${api.security.token.secret}")
     private String secret;
+    @Value("${api.security.token.issuer}")
+    private String issuer;
+
     public String gerarToken(Usuario usuario){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             Instant expiresAt = LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer(issuer)
                     .withSubject(usuario.getLogin())
 
                     .withClaim("id", usuario.getId())
@@ -30,6 +33,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt.", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 }
